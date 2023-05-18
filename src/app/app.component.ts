9d7e19ba30e869feb4b2e1d8660ac2ce966b3c3c
inv-sooraj <88436089+inv-sooraj@users.noreply.github.com>
2023-05-18 17:41:07 +0530

    Added ESLint validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    title = 'angularClient'
+export class AppComponent implements OnInit {
+    title = 'angularClient';
+    a = 1;
+    b = 2;
+
+    ngOnInit(): void {
+        /* TODO document why this method 'ngOnInit' is empty */
+        console.log('Will throw an error if the method is empty');
+    }
+
+    sum(a: number, b: number) {
+        a === b;
+    }
 }
